67b48a5025762cfe49ae6fbfe233ef3d68930017
olaolsso <olaolsso@users.noreply.github.com>
2016-04-15 18:56:02 +0200

    First commit

---
+++ blink1-folder.pl
@@ -0,0 +1,72 @@
+#!/usr/bin/perl
+
+use strict;
+use warnings;
+
+use Mail::IMAPClient;
+use Time::HiRes qw(usleep);
+use POSIX qw(strftime);
+use File::Find;
+
+my @folders = (
+	"/a/directory/to_watch"
+);
+
+my $blink1tool = "./blink1-tool";
+my $error_string = "--rgb 400000";
+
+sub dprint {
+	my ($string) = @_;
+	my $time = strftime("%F %T", localtime(time()));
+	print("$time $0: $string\n");
+}
+
+sub get_blink_string {
+	my $r = qx($blink1tool --rgbread);
+	$r =~ /(0x..,0x..,0x..)$/ or die;
+	return $1;
+}
+
+sub set_blink_string {
+	my ($string) = @_;
+	system("$blink1tool --quiet --millis 0 --nogamma $string") == 0 or die;
+}
+
+set_blink_string("--off");
+
+my @ls_last;
+
+while (1) {
+	my @ls;
+
+	foreach my $f (@folders) {
+		find({preprocess => sub {
+			return grep { -f || (-d && /^[^.]/) } @_;
+		}, wanted => sub {
+			push @ls, $_ if (-f);
+		}}, $f);
+	}
+	
+	@ls_last = @ls if !@ls_last;
+	
+	foreach my $l (@ls) {
+		my $is_found = 0;
+		foreach my $l_last (@ls_last) {
+			if ($l eq $l_last) {
+				$is_found = 1;
+				last;
+			}
+		}
+		if (!$is_found) {
+			my $bs = get_blink_string();
+			set_blink_string("--rgb ff00ff");
+			sleep(2);
+			set_blink_string("--rgb $bs");
+			last;
+		}
+	}
+
+	@ls_last = @ls;
+
+	sleep(90);
+}
---
+++ blink1-imap.pl
@@ -0,0 +1,140 @@
+#!/usr/bin/perl
+
+use strict;
+use warnings;
+
+use Mail::IMAPClient;
+use Time::HiRes qw(usleep);
+use POSIX qw(strftime);
+
+my $imap_server = "imap.gmail.com";
+my $imap_port = 993;
+my $imap_ssl = 1;
+my $imap_user = "yourusername";
+my $imap_password = "yourpassword";
+
+my $blink1tool = "./blink1-tool";
+my $error_string = "--rgb 400000";
+
+my @actions = (
+	{
+		"pattern" => "^from:.*do-not-reply\@stackexchange\.com",
+		"action" => "--rgb 004040"
+	},
+	{
+		"pattern" => "^(to|cc|bcc):.*yourusername\@gmail\.com",
+		"action" => "--rgb 000040"
+	},
+	{
+		"pattern" => ".*",
+		"action" => "--off"
+	}
+);
+
+sub dprint {
+	my ($string) = @_;
+	my $time = strftime("%F %T", localtime(time()));
+	print("$time $0: $string\n");
+}
+
+sub get_blink_string {
+	my $r = qx($blink1tool --rgbread);
+	$r =~ /(0x..,0x..,0x..)$/ or die;
+	return $1;
+}
+
+sub set_blink_string {
+	my ($string) = @_;
+	system("$blink1tool --quiet --millis 0 --nogamma $string") == 0 or die;
+}
+
+sub set_blink_error {
+	set_blink_string($error_string);
+}
+
+set_blink_error();
+
+my $imap;
+my @unseen_last;
+
+while (1) {
+	# disconnect
+	if ($imap && $imap->IsConnected()) {
+		$imap->logout();
+	}
+	undef $imap;
+
+	# connect
+	my $c_connection_attempts = 0;
+	until ($imap) {
+		dprint("connecting");
+		$imap = Mail::IMAPClient->new(
+			Server => $imap_server,
+			Port => $imap_port,
+			SSL => $imap_ssl,
+			Keepalive => 1,
+			Reconnectretry => 10,
+			User => $imap_user,
+			Password => $imap_password) or warn;
+
+		$c_connection_attempts++;
+		if ($c_connection_attempts > 1) {
+			set_blink_error();
+			sleep(60);
+		}
+		if ($c_connection_attempts > 10) {
+			sleep(20*60);
+		}
+	}
+	$imap->select("INBOX") or warn and next;
+	dprint("connected");
+
+	# count unread
+	while (1) {
+		my @unseen = $imap->unseen();
+
+		# set colour according to unseen message classes
+		if ($unseen[0]) {
+			my @headers = $imap->fetch(join(",", @unseen), "RFC822.HEADER") or warn and last;
+			my $headers = join("", @headers);
+			$headers =~ s/\r\n[ \t]/ /sg; # unfold folded header fields to single-line fields
+
+			foreach my $a (@actions) {
+				if ($headers =~ /$a->{"pattern"}/mi) {
+					set_blink_string($a->{"action"});
+					last;
+				}
+			}
+		} else {
+			set_blink_string("--off");
+		}
+
+		# flash once for new unseen messages
+		foreach my $u (@unseen) {
+			my $is_in_last = 0;
+			foreach my $ul (@unseen_last) {
+				if ($u == $ul) {
+					$is_in_last = 1;
+					last;
+				}
+			}
+			if (!$is_in_last) {
+				my $bs = get_blink_string();
+				set_blink_string("--rgb ffffff");
+				usleep(100*1000);
+				set_blink_string("--rgb $bs");
+				last;
+			}
+		}
+		@unseen_last = @unseen;
+
+		# can't ask imap server for unseen count between idle and done calls,
+		# thus race condition is possible. instead, idle_data must time out
+		# periodically to verify unseen count.
+
+		# wait for new activity
+		my $idle_tag = $imap->idle() or warn and last;
+		$imap->idle_data(660) or warn and last;
+		$imap->done($idle_tag) or warn and last;
+	}
+}
